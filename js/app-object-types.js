1f254f90d3e1b4973455fdf6de1260a41b9e4e6a
Christian Villareal <christian.villareal@mycbscebu.com>
2024-01-20 21:31:18 +0800

    partial with advanced obeject types

--- js/app-object-types.js
+++ js/app-object-types.js
@@ -1,5 +1,23 @@
+//** DECLARE AND CALL OBJECT TYPES **//
+var userRole;
+(function (userRole) {
+    userRole[userRole["ADMIN"] = 0] = "ADMIN";
+    userRole[userRole["MODERATOR"] = 1] = "MODERATOR";
+    userRole[userRole["USER"] = 2] = "USER";
+})(userRole || (userRole = {}));
 var student = {
     name: "christian",
-    age: 26
+    age: 26,
+    gender: "male",
+    interest: ["Basketball", "Reading"],
+    newAccess: userRole.ADMIN
 };
-console.table(student.age);
+if (student.newAccess === userRole.ADMIN) {
+    console.log("is Admin");
+}
+else if (student.newAccess === userRole.MODERATOR) {
+    console.log("is Moderator");
+}
+else {
+    console.log("is User");
+}
--- js/app-object-types.ts
+++ js/app-object-types.ts
@@ -1,8 +1,25 @@
 
 //** DECLARE AND CALL OBJECT TYPES **//
-const student = {
+enum userRole {ADMIN, MODERATOR, USER}
+
+const student: {
+    name: string;
+    age: number;
+    gender?: string;
+    interest: string[]; // declaring Array also if you want to declare number and string use any[] instead of string[]
+    newAccess: userRole;
+} = {
     name: "christian",
-    age: 26
+    age: 26,
+    gender: "male",
+    interest: ["Basketball", "Reading"],
+    newAccess: userRole.ADMIN
 }
 
-console.log(student.age)
+if(student.newAccess === userRole.ADMIN) {
+    console.log("is Admin")
+} else if(student.newAccess === userRole.MODERATOR) {
+    console.log("is Moderator")
+} else {
+    console.log("is User")
+}
